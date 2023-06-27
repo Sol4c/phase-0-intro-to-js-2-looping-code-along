b0d45498cdd9c270b9d845278d5961eac3509bd3
Sol4c <brownarmani86@yahoo.com>
2023-06-26 22:26:31 -0400

    :(

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solutions in this file
+
+const greetings = []
+
+
+function writeCards(names){
+    
+    for (let n = 0; n < names.length; n++){
+        console.log(`Thank you, ${names[n]}, for the wonderful surprise gift!`);
+        greetings.push(`Thank you, ${names[n]}, for the wonderful surprise gift!`)
+    }
+    return greetings
+
+}
+
+function countDown(number){
+    
+    for (; number >= 0; number--){
+    console.log(number)
+    }
+}
+
+
+
